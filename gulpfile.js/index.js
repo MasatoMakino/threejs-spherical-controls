18f3c850d55679fd42decc64f7a960c35b1a506f
MasatoMakino <unagiinu.g@gmail.com>
2019-12-12 16:06:49 +0900

    update : devDependencies

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -4,16 +4,18 @@ const { series } = require("gulp");
 
 const doc = require("gulptask-tsdoc").get();
 const server = require("gulptask-dev-server").get("./docs/demo");
-const { bundleDevelopment, watchBundle } = require("gulptask-webpack")(
-  "./webpack.config.js"
-);
+const { bundleDemo, watchDemo } = require("gulptask-demo-page").get({
+  externalScripts: [
+    "//cdnjs.cloudflare.com/ajax/libs/tweenjs/1.0.2/tweenjs.min.js"
+  ],
+  body: `<canvas id="webgl-canvas" width="1920" height="1080"></canvas>`
+});
 const { tsc, watchTsc } = require("gulptask-tsc").get();
 
-const watchTasks = cb => {
-  watchBundle();
+const watchTasks = async () => {
+  watchDemo();
   watchTsc();
-  cb();
 };
 
 exports.start_dev = series(watchTasks, server);
-exports.build = series(tsc, bundleDevelopment, doc);
+exports.build = series(tsc, bundleDemo, doc);
