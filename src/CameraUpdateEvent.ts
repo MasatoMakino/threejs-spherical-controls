af7dbbb69c1f3f1fd320cc5362925f65405265c4
MasatoMakino <makino.masato.g@gmail.com>
2023-03-09 11:32:24 +0900

    add : comment

--- src/CameraUpdateEvent.ts
+++ src/CameraUpdateEvent.ts
@@ -1,5 +1,11 @@
 import { Event, Mesh, Spherical, Vector3 } from "three";
 
+/**
+ * カメラ位置が更新された場合に発行されるイベント
+ * RAFで間引きされて発行される。
+ * レンダリングを間引く目的などで利用される。
+ * TODO : SphericalControllerEventとCameraUpdateEventを一本化できないか検討する
+ */
 export interface CameraUpdateEvent extends Event {
   type: CameraUpdateEventType;
   cameraTarget: Mesh;
