b9da61dd4177fcfcee26bc2c16f7ae1e87bff202
MasatoMakino <makino.masato.g@gmail.com>
2023-03-09 11:16:29 +0900

    add : comment

--- src/SphericalControllerEvent.ts
+++ src/SphericalControllerEvent.ts
@@ -1,6 +1,13 @@
 import { SphericalParamType, TargetParam } from "./TargetParam";
 import { Event } from "three";
 
+/**
+ * カメラが移動した際に発行されるイベント
+ * 主にSphericalController内で利用される。
+ *
+ * SphericalController外でカメラの移動を検知するためにはCameraUpdateEventを利用する。
+ * TODO : SphericalControllerEventとCameraUpdateEventを一本化できないか検討する
+ */
 export interface SphericalControllerEvent extends Event {
   type: SphericalControllerEventType;
   completedParam?: TargetParam | SphericalParamType;
