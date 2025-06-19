64e5e3cdc02e78016c864032016aa116c85a279a
MasatoMakino <makino.masato.g@gmail.com>
2025-06-19 17:54:17 +0900

    fix: mark SphericalControllerUtil as deprecated and suggest named exports

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,9 @@ export * from "./CameraUpdateEvent.js";
 export * from "./CameraPositionUpdater.js";
 export * from "./CameraPositionLimiter.js";
 export * from "./SphericalControllerTween.js";
+
+import * as SphericalControllerUtil from "./SphericalControllerUtil.js";
+/**
+ * @deprecated Use named exports directly from "@masatomakino/threejs-spherical-controls" instead.
+ */
+export { SphericalControllerUtil };
